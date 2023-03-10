82dec637b794b8eb399a9626830a6f12774016be
Kilya265 <liliya.nikodimova@yandex.ru>
2023-03-10 20:10:19 +0700

    Проектная работа 4. Место

---
+++ scripts/srcript.js
@@ -0,0 +1,36 @@
+let popupElement = document.querySelector('.popup__edit-form');
+
+let popupCloseButtonElement = popupElement.querySelector('.popup__close');
+let popupOpenButtonElement = document.querySelector('.profile__edit-button');
+
+let titleProfile = document.querySelector('.profile__title');
+let titlePopup = document.querySelector('.popup__input_key_title');
+
+let descriptionProfile = document.querySelector('.profile__description');
+let descriptionPopup = document.querySelector('.popup__input_key_description');
+
+let saveButtonElement = document.querySelector('.popup__save');
+
+const openPopup = function () {
+  popupElement.classList.toggle('popup_is-opened');
+  titlePopup.value = titleProfile.textContent;
+  descriptionPopup.value = descriptionProfile.textContent;
+}
+popupOpenButtonElement.addEventListener('click', openPopup);
+
+const closePopup = function () {
+  popupElement.classList.remove('popup_is-opened');
+}
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+
+function saveButtonEdit(saveButton) {
+  saveButton.preventDefault()
+
+  titleProfile.textContent = titlePopup.value;
+  descriptionProfile.textContent = descriptionPopup.value;
+
+  popupElement.classList.remove('popup_is-opened');
+}
+
+popupElement.addEventListener('submit', saveButtonEdit);
